fa20e2b24be96d171597b43e5c2e872e56fd8890
gregoriussamuelh@gmail.com <gregoriussamuelh@gmail.com>
2024-04-30 12:39:51 +0700

    Payment Service Implementation

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-    .build_Server(true)
-    .compile(
-        &["proto/services.proto"], // Path to your proto file
-        &["proto"], // Directory where the proto file is located
-    )?;
+        .build_server(true)
+        .compile(
+            &["proto/services.proto"], // Path to your proto file
+            &["proto"], // Directory where the proto file is located
+        )?;
     Ok(())
 }
